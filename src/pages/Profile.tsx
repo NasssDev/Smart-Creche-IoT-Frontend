ecd6eec65c944ee8b89dc2268b405e93b46e977e
Ach <chardoudiachraf@gmail.com>
2023-07-17 11:29:09 +0200

    ProfileCard on Profile Page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,11 @@
 import {LabelPage} from "../components/LabelPage.tsx";
+import ProfileCard from "../components/ProfileCard";
 
 export const Profile = () => {
     return (
         <>
             <LabelPage label={"Profile"} />
+            <ProfileCard/>
         </>
     )
 }
