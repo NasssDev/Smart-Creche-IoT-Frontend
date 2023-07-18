c80ebdecc3bc33ee3ef6eeb362d14e2800fda3de
Fane Yaya <y.fane75@hotmail.fr>
2023-07-18 16:19:48 +0200

    calandar yaya

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,12 +1,13 @@
 import {LabelPage} from "../components/LabelPage.tsx";
+import { TabEvents } from "../components/TabEvents.tsx";
 // import {DegreeBar} from "../components/DegreeBar.tsx";
 
 export const Events = () => {
     return (
         <>
             <LabelPage label={"Events"} />
-            <div className="flex justify-center items-center h-screen">
-                {/*<DegreeBar temperature={55} />*/}
+            <div className="flex justify-center h-screen">
+                <TabEvents/>
             </div>
         </>
     )
