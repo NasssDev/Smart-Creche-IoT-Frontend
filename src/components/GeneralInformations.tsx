c67718d6750a434037f059f5c83abfc51582600b
Ach <chardoudiachraf@gmail.com>
2023-07-19 12:33:21 +0200

    Using ant.design on the form of GeneralInformations

--- src/components/GeneralInformations.tsx
+++ src/components/GeneralInformations.tsx
@@ -1,90 +1,124 @@
-import { Card } from 'antd';
+import React from 'react';
+import { Card, Input } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 
 const GeneralInformations = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Handle form submission logic here
+    };
+
     return (
-            <Card className="w-400 rounded-3xl shadow-lg">
-                <h2 className="text-3xl">General Information</h2>
-
-                <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
-                    <div className="-mx-3 md:flex mb-6">
-                        <div className="md:w-full px-3">
-                            <label className="block uppercase tracking-wide text-black-700 text-xs font-bold mb-2" htmlFor="grid-email">
-                                Email
-                            </label>
-                            <input
-                                className="appearance-none block w-full bg-blue-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3"
-                                id="grid-email"
-                                type="email"
-                                placeholder="username@example.com"
-                            />
-                        </div>
-                    </div>
-
-                    <div className="-mx-3 md:flex mb-6">
-                        <div className="md:w-1/2 px-3">
-                            <label className="block uppercase tracking-wide text-black-700 text-xs font-bold mb-2" htmlFor="grid-first-name" >
-                                First Name
-                            </label>
-                            <input
-                                className="appearance-none block w-full bg-blue-200 text-gray-700 border border-gray-200 rounded py-3 px-4"
-                                id="grid-last-name"
-                                type="text"
-                                placeholder="Adela"
-                            />
-                        </div>
-                        <div className="md:w-1/2 px-3">
-                            <label className="block uppercase tracking-wide text-black-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
-                                Last Name
-                            </label>
-                            <input
-                                className="appearance-none block w-full bg-blue-200 text-gray-700 border border-gray-200 rounded py-3 px-4"
-                                id="grid-last-name"
-                                type="text"
-                                placeholder="Parkson"
-                            />
-                        </div>
-                    </div>
-                    <div className="-mx-3 md:flex mb-6">
-                        <div className="md:w-full px-3">
-                            <label className="block uppercase tracking-wide text-black-700 text-xs font-bold mb-2" htmlFor="grid-positionheld">
-                                Position held
-                            </label>
-                            <input
-                                className="appearance-none block w-full bg-blue-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3"
-                                id="grid-positionheld"
-                                type="text"
-                                placeholder="Directrice"
-                            />
-                        </div>
-                    </div>
-                    <div className="-mx-3 md:flex mb-6">
-                        <div className="md:w-full px-3">
-                            <label className="block uppercase tracking-wide text-black-700 text-xs font-bold mb-2" htmlFor="grid-collectivecrech">
-                                Siret the collective crech
-                            </label>
-                            <input
-                                className="appearance-none block w-full bg-blue-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3"
-                                id="grid-collectivecrech"
-                                type="number"
-                                placeholder="49336137200011"
-                            />
-                        </div>
-                    </div>
-                    <div className="-mx-3 md:flex mb-6">
-                        <div className="md:w-full px-3">
-                            <label className="block uppercase tracking-wide text-black-700 text-xs font-bold mb-2" htmlFor="grid-password">
-                                Password
-                            </label>
-                            <input
-                                className="appearance-none block w-full bg-blue-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3"
-                                id="grid-password"
-                                type="password"
-                                placeholder="***************"
-                            />
-                        </div>
-                    </div>
+        <Card className="w-400 rounded-3xl shadow-lg">
+            <h2 className="text-3xl">General Information</h2>
+            &nbsp;
+            &nbsp;
+            <form onSubmit={handleSubmit}>
+                <div className="mb-2">
+                    <label
+                        className="block uppercase tracking-wide text-black text-xs font-bold mb-2"
+                        htmlFor="grid-email"
+                    >
+                        Email
+                    </label>
+                    <Input
+                        size="small"
+                        className="text-gray-700 rounded py-1 px-3"
+                        id="grid-email"
+                        type="email"
+                        placeholder="username@example.com"
+                    />
                 </div>
-            </Card>
+
+                <div className="mb-2">
+                    <label
+                        className="block uppercase tracking-wide text-black text-xs font-bold mb-2"
+                        htmlFor="grid-first-name"
+                    >
+                        First Name
+                    </label>
+                    <Input
+                        size="small"
+                        className="text-gray-700 rounded py-1 px-3"
+                        id="grid-first-name"
+                        type="text"
+                        placeholder="Adela"
+                    />
+                </div>
+
+                <div className="mb-2">
+                    <label
+                        className="block uppercase tracking-wide text-black text-xs font-bold mb-2"
+                        htmlFor="grid-last-name"
+                    >
+                        Last Name
+                    </label>
+                    <Input
+                        size="small"
+                        className="text-gray-700 rounded py-1 px-3"
+                        id="grid-last-name"
+                        type="text"
+                        placeholder="Parkson"
+                    />
+                </div>
+
+                <div className="mb-2">
+                    <label
+                        className="block uppercase tracking-wide text-black text-xs font-bold mb-2"
+                        htmlFor="grid-positionheld"
+                    >
+                        Position held
+                    </label>
+                    <Input
+                        size="small"
+                        className="text-gray-700 rounded py-1 px-3"
+                        id="grid-positionheld"
+                        type="text"
+                        placeholder="Directrice"
+                    />
+                </div>
+
+                <div className="mb-2">
+                    <label
+                        className="block uppercase tracking-wide text-black text-xs font-bold mb-2"
+                        htmlFor="grid-collectivecrech"
+                    >
+                        Siret the collective crech
+                    </label>
+
+                    <Input
+                        size="small"
+                        className="text-gray-700 rounded py-1 px-3"
+                        id="grid-collectivecrech"
+                        type="number"
+                        placeholder="49336137200011"
+                    />
+                </div>
+
+                <div className="mb-2">
+                    <label
+                        className="block uppercase tracking-wide text-black text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                    >
+                        Password
+                    </label>
+                    <Input.Password
+                        size="small"
+                        className="text-gray-700 rounded py-1 px-3"
+                        id="grid-password"
+                        placeholder="***************"
+                    />
+                </div>
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
+                    type="submit"
+                >
+                    Modify
+                </button>
+
+            </form>
+        </Card>
     );
 };
 
